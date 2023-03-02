4880f50bdac1e997cc4e7a72be4cfcbc2e5f8f5f
Duks31 <chidubemjan31@gmail.com>
2023-03-02 13:33:25 +0100

    fibonacci generator

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+// Generating the nth fibonnaci number
+use std::io;
+
+fn fibonnaci(n: u32) -> u32 {
+    if n == 0{
+        0
+    } else if n == 1 {
+        1
+    } else {
+        fibonnaci(n-1) + fibonnaci(n-2)
+    }
+}
+
+fn main() {
+    println!("Generating Fibonnaci numbers. ");
+    println!("Enter a number: ");
+
+    let mut num = String::new();
+    io::stdin().read_line(&mut num).expect("Failed to read line");
+
+    let num: u64 = match num.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid Input");
+            return;
+        }
+    };
+    let answer = fibonnaci(num.try_into().unwrap());
+
+    println!("Fibonacci number at position {num} is: {answer}");
+}
